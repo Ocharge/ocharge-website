cc6711267f378193f10440e12856517d8a641241
Petermuturi <pmuturi65@gmail.com>
2016-09-13 20:48:14 +0300

    added new js file

---
+++ javascript/home-nav.js
@@ -0,0 +1,15 @@
+$('.nav').hide();
+$(document).ready(function() {
+	function showNav() {
+		if ($(this).scrollTop() > 100) {
+			$('.nav-home').fadeOut(500);
+			$('.nav').fadeIn(500);
+		}
+		else {
+			$('.nav').fadeOut(500);
+			$('.nav-home').fadeIn(500);
+		}
+	}
+	$(window).scroll(showNav);
+	showNav();
+});
