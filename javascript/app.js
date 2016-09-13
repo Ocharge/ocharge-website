8b08ed483eaa7b8231f96b1708610fe70578d633
Petermuturi <pmuturi65@gmail.com>
2016-09-13 20:45:40 +0300

    added color change on nav as it scrolls

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,16 @@
-$('.red-bg').hide();
+$('.nav').hide();
+$(document).ready(function() {
+	function showNav() {
+		if ($(this).scrollTop() > 100) {
+			$('.nav-home').fadeOut(500);
+			$('.nav').fadeIn(500);
+		}
+		else {
+			$('.nav').fadeOut(500);
+			$('.nav-home').fadeIn(500);
+		}
+	}
+	$(window).scroll(showNav);
+
+	showNav();
+});
