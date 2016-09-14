a48e1893930fff6fa151c2667e1e40936207e284
Petermuturi <pmuturi65@gmail.com>
2016-09-14 11:56:16 +0300

    completed navigation

---
+++ javascript/app.js
@@ -0,0 +1,12 @@
+// Nav hamburger menu
+  $(function() {
+    $( ".icn" ).click(function() {
+      $( ".hide-nav" ).toggleClass( "show-nav", 400 );
+    });
+  });
+  // Nav hamburger menu
+  $(function() {
+    $( ".icn-home" ).click(function() {
+      $( ".hide-nav" ).toggleClass( "show-nav", 400 );
+    });
+  });
