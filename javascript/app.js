1bc02c359165d9978a720f2a11535ac4fa8de7d5
Petermuturi <pmuturi65@gmail.com>
2016-09-14 14:26:43 +0300

    completed sectio two

--- javascript/app.js
+++ javascript/app.js
@@ -1,12 +1,47 @@
 // Nav hamburger menu
-  $(function() {
-    $( ".icn" ).click(function() {
-      $( ".hide-nav" ).toggleClass( "show-nav", 400 );
-    });
+$(function() {
+  $( ".icn" ).click(function() {
+    $( ".hide-nav" ).toggleClass( "show-nav", 400 );
   });
-  // Nav hamburger menu
-  $(function() {
-    $( ".icn-home" ).click(function() {
-      $( ".hide-nav" ).toggleClass( "show-nav", 400 );
-    });
+});
+// Nav-home hamburger menu
+$(function() {
+  $( ".icn-home" ).click(function() {
+    $( ".hide-nav" ).toggleClass( "show-nav", 400 );
   });
+});
+$('#points').hide();
+$('#deals').hide();
+// points
+$(function() {
+  $( ".points" ).click(function() {
+    $( "#points" ).fadeIn( 300 );
+    $('#recharge').fadeOut(300);
+    $("#deals").fadeOut(300);
+    $(".off-po").addClass("on-po", 300);
+    $(".off-re").removeClass("on-re", 300);
+    $(".off-de").removeClass("on-de", 300);
+  });
+});
+// deals
+$(function() {
+  $( ".deals" ).click(function() {
+    $( "#deals" ).fadeIn( 300 );
+    $('#recharge').fadeOut(300);
+    $("#points").fadeOut(300);
+    $(".off-de").addClass("on-de", 300);
+    $(".off-re").removeClass("on-re", 300);
+    $(".off-po").removeClass("on-po", 300);
+  });
+});
+// Recharge
+$(function() {
+  $( ".recharge" ).click(function() {
+    $( "#recharge" ).fadeIn( 300 );
+    $('#deals').fadeOut(300);
+    $("#points").fadeOut(300);
+    $(".off-re").addClass("on-re", 300);
+    $(".off-de").removeClass("on-de", 300);
+    $(".off-po").removeClass("on-po", 300);
+  });
+});
