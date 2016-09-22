53a3f6f9f492e292bb17c3289a28fddb04c3b71b
Petermuturi <pmuturi65@gmail.com>
2016-09-22 11:45:52 +0300

    added pre-filled value

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,17 @@
+// ************
+// Prefilled +254 on input field
+// ************
+$(document).ready(function(){
+	$("#phone").keydown(function(e) {
+	    var oldvalue=$(this).val();
+	    var field=this;
+	    setTimeout(function () {
+	        if(field.value.indexOf('+254') !== 0) {
+	            $(field).val(oldvalue);
+	        } 
+	    }, 1);
+	});
+});
 // Nav hamburger menu
 $(function() {
   $( ".icn" ).click(function() {
